d3194b147d623e8e7fb53798d3cbcc9485ba49cd
hengboy <jnyuqy@gmail.com>
2022-11-30 16:28:10 +0800

    :tada: 初始化数据存储库接口以及JDBC实现类

---
+++ on-security-core/src/main/java/org/minbox/framework/on/security/core/authorization/data/client/SecurityClientScopeJdbcRepository.java
@@ -0,0 +1,35 @@
+/*
+ *     Copyright (C) 2022  恒宇少年
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.minbox.framework.on.security.core.authorization.data.client;
+
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.util.Assert;
+
+/**
+ * 客户端范围存储库JDBC实现类
+ *
+ * @author 恒宇少年
+ */
+public class SecurityClientScopeJdbcRepository implements SecurityClientScopeRepository {
+    private JdbcOperations jdbcOperations;
+
+    public SecurityClientScopeJdbcRepository(JdbcOperations jdbcOperations) {
+        Assert.notNull(jdbcOperations, "jdbcOperations cannot be null");
+        this.jdbcOperations = jdbcOperations;
+    }
+}
