37c2e59d9cf05334d55a19e2e92ed70a173a7ee5
hengboy <jnyuqy@gmail.com>
2022-12-04 16:19:51 +0800

    :rocket: 提供On-SecurityJDBC方式的OAuth2AuthorizationService数据存储类

---
+++ on-security-core/src/main/java/org/minbox/framework/on/security/core/authorization/data/session/converter/OAuth2AuthorizationToSecuritySessionConverter.java
@@ -0,0 +1,99 @@
+/*
+ *     Copyright (C) 2022  恒宇少年
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.minbox.framework.on.security.core.authorization.data.session.converter;
+
+import org.minbox.framework.on.security.core.authorization.data.session.SecuritySession;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2RefreshToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationCode;
+import org.springframework.util.ObjectUtils;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+/**
+ * 将{@link OAuth2Authorization}转换为{@link SecuritySession}
+ *
+ * @author 恒宇少年
+ * @see OAuth2Authorization
+ * @see SecuritySession
+ * @since 0.0.1
+ */
+public final class OAuth2AuthorizationToSecuritySessionConverter implements Converter<OAuth2Authorization, SecuritySession> {
+    @Override
+    public SecuritySession convert(OAuth2Authorization authorization) {
+        String authorizationId = authorization.getId();
+        // @formatter:off
+        SecuritySession.Builder builder = SecuritySession.withId(authorizationId)
+                .username(authorization.getPrincipalName())
+                .attributes(authorization.getAttributes())
+                .authorizationGrantType(authorization.getAuthorizationGrantType())
+                .authorizationScopes(authorization.getAuthorizedScopes());
+        // @formatter:on
+        // Convert OAuth2AuthorizationCode
+        OAuth2Authorization.Token<OAuth2AuthorizationCode> authorizationCodeToken = authorization.getToken(OAuth2AuthorizationCode.class);
+        if (!ObjectUtils.isEmpty(authorizationCodeToken)) {
+            OAuth2AuthorizationCode oAuth2AuthorizationCode = authorizationCodeToken.getToken();
+            // @formatter:off
+            builder.authorizationCodeValue(oAuth2AuthorizationCode.getTokenValue())
+                    .authorizationCodeIssuedAt(LocalDateTime.ofInstant(oAuth2AuthorizationCode.getIssuedAt(), ZoneId.systemDefault()))
+                    .authorizationCodeExpiresAt(LocalDateTime.ofInstant(oAuth2AuthorizationCode.getExpiresAt(),ZoneId.systemDefault()))
+                    .authorizationCodeMetadata(authorizationCodeToken.getMetadata());
+            // @formatter:on
+        }
+        // Convert OAuth2AccessToken
+        OAuth2Authorization.Token<OAuth2AccessToken> authorizationAccessToken = authorization.getToken(OAuth2AccessToken.class);
+        if (!ObjectUtils.isEmpty(authorizationAccessToken)) {
+            OAuth2AccessToken accessToken = authorizationAccessToken.getToken();
+            // @formatter:off
+            builder.accessTokenValue(accessToken.getTokenValue())
+                    .accessTokenType(accessToken.getTokenType())
+                    .accessTokenIssuedAt(LocalDateTime.ofInstant(accessToken.getIssuedAt(), ZoneId.systemDefault()))
+                    .accessTokenExpiresAt(LocalDateTime.ofInstant(accessToken.getExpiresAt(), ZoneId.systemDefault()))
+                    .accessTokenMetadata(authorizationAccessToken.getMetadata());
+            // @formatter:on
+        }
+        // Convert OidcIdToken
+        OAuth2Authorization.Token<OidcIdToken> authorizationOidcIdToken = authorization.getToken(OidcIdToken.class);
+        if (!ObjectUtils.isEmpty(authorizationOidcIdToken)) {
+            OidcIdToken oidcIdToken = authorizationOidcIdToken.getToken();
+            // @formatter:off
+            builder.oidcIdTokenValue(oidcIdToken.getTokenValue())
+                    .oidcIdTokenIssuedAt(LocalDateTime.ofInstant(oidcIdToken.getIssuedAt(), ZoneId.systemDefault()))
+                    .oidcIdTokenExpiresAt(LocalDateTime.ofInstant(oidcIdToken.getExpiresAt(), ZoneId.systemDefault()))
+                    .oidcIdTokenMetadata(authorizationOidcIdToken.getMetadata());
+            // @formatter:on
+        }
+
+        // Convert OAuth2RefreshToken
+        OAuth2Authorization.Token<OAuth2RefreshToken> authorizationRefreshToken = authorization.getRefreshToken();
+        if (!ObjectUtils.isEmpty(authorizationRefreshToken)) {
+            OAuth2RefreshToken refreshToken = authorizationRefreshToken.getToken();
+            // @formatter:off
+            builder.refreshTokenValue(refreshToken.getTokenValue())
+                    .refreshTokenIssuedAt(LocalDateTime.ofInstant(refreshToken.getIssuedAt(), ZoneId.systemDefault()))
+                    .refreshTokenExpiresAt(LocalDateTime.ofInstant(refreshToken.getExpiresAt(), ZoneId.systemDefault()))
+                    .refreshTokenMetadata(authorizationRefreshToken.getMetadata());
+            // @formatter:on
+        }
+        return builder.build();
+    }
+}
---
+++ on-security-core/src/main/java/org/minbox/framework/on/security/core/authorization/data/session/converter/SecuritySessionToOAuth2AuthorizationConverter.java
@@ -0,0 +1,117 @@
+/*
+ *     Copyright (C) 2022  恒宇少年
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.minbox.framework.on.security.core.authorization.data.session.converter;
+
+import org.minbox.framework.on.security.core.authorization.data.session.SecuritySession;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2RefreshToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationCode;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+
+import java.time.ZoneId;
+import java.util.Map;
+
+/**
+ * 将{@link SecuritySession}转换为{@link OAuth2Authorization}
+ *
+ * @author 恒宇少年
+ * @see SecuritySession
+ * @see OAuth2Authorization
+ * @since
+ */
+public class SecuritySessionToOAuth2AuthorizationConverter implements Converter<SecuritySession, OAuth2Authorization> {
+    @Override
+    public OAuth2Authorization convert(SecuritySession session) {
+        // @formatter:off
+        OAuth2Authorization.Builder builder = OAuth2Authorization
+                .withRegisteredClient(RegisteredClient.withId(session.getClientId()).build())
+                .id(session.getId())
+                .principalName(session.getUsername())
+                .authorizationGrantType(session.getAuthorizationGrantType())
+                .authorizedScopes(session.getAuthorizationScopes())
+                .attributes(attibuteMap -> attibuteMap.putAll(session.getAttributes()));
+        // @formatter:on
+
+        // Convert OAuth2AuthorizationCode
+        OAuth2AuthorizationCode auth2AuthorizationCode = new OAuth2AuthorizationCode(
+                session.getAuthorizationCodeValue(),
+                session.getAuthorizationCodeIssuedAt().atZone(ZoneId.systemDefault()).toInstant(),
+                session.getAuthorizationCodeExpiresAt().atZone(ZoneId.systemDefault()).toInstant());
+        if (!ObjectUtils.isEmpty(session.getAuthorizationCodeMetadata())) {
+            builder.token(auth2AuthorizationCode, (metadata) -> metadata.putAll(session.getAuthorizationCodeMetadata()));
+        } else {
+            builder.token(auth2AuthorizationCode);
+        }
+
+        // Convert OAuth2AccessToken
+        if (!ObjectUtils.isEmpty(session.getAccessTokenValue())) {
+            // @formatter:off
+            OAuth2AccessToken accessToken = new OAuth2AccessToken(
+                    session.getAccessTokenType(),
+                    session.getAccessTokenValue(),
+                    session.getAccessTokenIssuedAt().atZone(ZoneId.systemDefault()).toInstant(),
+                    session.getAccessTokenExpiresAt().atZone(ZoneId.systemDefault()).toInstant(),
+                    session.getAuthorizationScopes());
+            // @formatter:on
+            // Put all access token metadata
+            if (!ObjectUtils.isEmpty(session.getAccessTokenMetadata())) {
+                builder.token(accessToken, (metadata) -> metadata.putAll(session.getAccessTokenMetadata()));
+            } else {
+                builder.accessToken(accessToken);
+            }
+        }
+        // Convert OAuth2RefreshToken
+        if (!ObjectUtils.isEmpty(session.getRefreshTokenValue())) {
+            // @formatter:off
+            OAuth2RefreshToken refreshToken = new OAuth2RefreshToken(
+                    session.getRefreshTokenValue(),
+                    session.getRefreshTokenIssuedAt().atZone(ZoneId.systemDefault()).toInstant(),
+                    session.getRefreshTokenExpiresAt().atZone(ZoneId.systemDefault()).toInstant());
+            // @formatter:on
+            // Put all refresh token metadata
+            if (!ObjectUtils.isEmpty(session.getRefreshTokenMetadata())) {
+                builder.token(refreshToken, (metadata) -> metadata.putAll(session.getRefreshTokenMetadata()));
+            } else {
+                builder.refreshToken(refreshToken);
+            }
+        }
+        // Convert OidcIdToken
+        if (!ObjectUtils.isEmpty(session.getOidcIdTokenValue())) {
+            Map<String, Object> oidcIdTokenMetadata = session.getOidcIdTokenMetadata();
+            // @formatter:off
+            Map<String, Object> oidcIdTokenClaims =
+                    oidcIdTokenMetadata.containsKey(OAuth2Authorization.Token.CLAIMS_METADATA_NAME) ?
+                            (Map<String, Object>) oidcIdTokenMetadata.get(OAuth2Authorization.Token.CLAIMS_METADATA_NAME) :
+                            CollectionUtils.newHashMap(0);
+            OidcIdToken oidcIdToken = new OidcIdToken(
+                    session.getOidcIdTokenValue(),
+                    session.getOidcIdTokenIssuedAt().atZone(ZoneId.systemDefault()).toInstant(),
+                    session.getOidcIdTokenExpiresAt().atZone(ZoneId.systemDefault()).toInstant(),
+                    oidcIdTokenClaims);
+            // @formatter:on
+            // Put all oidc id token metadata
+            builder.token(oidcIdToken, (metadata) -> metadata.putAll(session.getOidcIdTokenMetadata()));
+        }
+        return builder.build();
+    }
+}
