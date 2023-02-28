3de37ff872abbf465b4bc5b72e679dbac7bf990a
hengboy <jnyuqy@gmail.com>
2023-02-28 13:24:48 +0800

    :sparkles: 提供"/on-security/manage/access/authorization"端点，用于外部服务获取"ManageToken"授权信息

---
+++ console/src/main/java/org/minbox/framework/on/security/console/authorization/config/configurer/support/OnSecurityManageTokenExternalAccessConfigurer.java
@@ -0,0 +1,69 @@
+/*
+ *   Copyright (C) 2022  恒宇少年
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.minbox.framework.on.security.console.authorization.config.configurer.support;
+
+import org.minbox.framework.on.security.console.authorization.authentication.OnSecurityManageTokenAccessAuthorizationProvider;
+import org.minbox.framework.on.security.console.authorization.web.OnSecurityManageTokenExternalAccessAuthorizationFilter;
+import org.minbox.framework.on.security.core.authorization.configurer.AbstractOnSecurityOAuth2Configurer;
+import org.minbox.framework.on.security.core.authorization.util.HttpSecuritySharedObjectUtils;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+/**
+ * 管理令牌（ManageToken）外部服务访问配置
+ *
+ * @author 恒宇少年
+ * @since 0.0.9
+ */
+public class OnSecurityManageTokenExternalAccessConfigurer extends AbstractOnSecurityOAuth2Configurer {
+    private static final String MANAGE_TOKEN_ACCESS_URI = "/on-security/manage/access/authorization";
+    private RequestMatcher MANAGE_TOKEN_ACCESS_REQUEST_MATCHER = new AntPathRequestMatcher(MANAGE_TOKEN_ACCESS_URI, HttpMethod.POST.name());
+
+    public OnSecurityManageTokenExternalAccessConfigurer(ObjectPostProcessor<Object> objectPostProcessor) {
+        super(objectPostProcessor);
+    }
+
+    @Override
+    public void init(HttpSecurity httpSecurity) {
+        // @formatter:off
+        OnSecurityManageTokenAccessAuthorizationProvider manageTokenAccessAuthorizationProvider
+                = new OnSecurityManageTokenAccessAuthorizationProvider(httpSecurity.getSharedObjects());
+        // @formatter:on
+        httpSecurity.authenticationProvider(manageTokenAccessAuthorizationProvider);
+    }
+
+    @Override
+    public RequestMatcher getRequestMatcher() {
+        return MANAGE_TOKEN_ACCESS_REQUEST_MATCHER;
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) {
+        AuthenticationManager authenticationManager = HttpSecuritySharedObjectUtils.getAuthenticationManager(httpSecurity);
+        // @formatter:off
+        OnSecurityManageTokenExternalAccessAuthorizationFilter manageTokenExternalAccessAuthorizationFilter =
+                new OnSecurityManageTokenExternalAccessAuthorizationFilter(MANAGE_TOKEN_ACCESS_REQUEST_MATCHER, authenticationManager);
+        // @formatter:on
+        httpSecurity.addFilterAfter(this.postProcess(manageTokenExternalAccessAuthorizationFilter), LogoutFilter.class);
+    }
+}
---
+++ console/src/main/java/org/minbox/framework/on/security/console/authorization/web/OnSecurityManageTokenExternalAccessAuthorizationFilter.java
@@ -0,0 +1,101 @@
+/*
+ *   Copyright (C) 2022  恒宇少年
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.minbox.framework.on.security.console.authorization.web;
+
+import org.minbox.framework.on.security.authorization.server.oauth2.authentication.OnSecurityDefaultAuthenticationFailureHandler;
+import org.minbox.framework.on.security.console.authorization.authentication.OnSecurityManageTokenAccessRequestAuthorizationToken;
+import org.minbox.framework.on.security.console.authorization.web.converter.OnSecurityManageTokenAccessAuthorizationConvert;
+import org.minbox.framework.on.security.core.authorization.jackson2.OnSecurityJsonMapper;
+import org.minbox.framework.on.security.core.authorization.manage.ManageTokenAccessAuthorization;
+import org.minbox.framework.on.security.core.authorization.manage.context.OnSecurityManageContextHolder;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 管理令牌（ManageToken）外部服务访问的授权认证过滤器
+ * <p>
+ * 携带管理令牌（ManageToken）访问"/on-security/manage/access/authorization"时，获取携带管理令牌的所授权的信息
+ *
+ * @author 恒宇少年
+ * @since 0.0.9
+ */
+public class OnSecurityManageTokenExternalAccessAuthorizationFilter extends OncePerRequestFilter {
+    private RequestMatcher requestMatcher;
+    private AuthenticationManager authenticationManager;
+    private AuthenticationSuccessHandler successHandler;
+    private AuthenticationFailureHandler failureHandler;
+    private AuthenticationConverter authenticationConverter;
+
+    public OnSecurityManageTokenExternalAccessAuthorizationFilter(RequestMatcher requestMatcher, AuthenticationManager authenticationManager) {
+        this.requestMatcher = requestMatcher;
+        this.authenticationManager = authenticationManager;
+        this.successHandler = new ManageTokenExternalAccessAuthorizationSuccessHandler();
+        this.failureHandler = new OnSecurityDefaultAuthenticationFailureHandler();
+        this.authenticationConverter = new OnSecurityManageTokenAccessAuthorizationConvert();
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (!this.requestMatcher.matches(request)) {
+            filterChain.doFilter(request, response);
+        } else {
+            try {
+                // @formatter:off
+                OnSecurityManageTokenAccessRequestAuthorizationToken manageTokenAccessRequestAuthorizationToken =
+                        (OnSecurityManageTokenAccessRequestAuthorizationToken) this.authenticationConverter.convert(request);
+                Authentication authentication = this.authenticationManager.authenticate(manageTokenAccessRequestAuthorizationToken);
+                this.successHandler.onAuthenticationSuccess(request, response, authentication);
+                // @formatter:on
+            } catch (AuthenticationException e) {
+                failureHandler.onAuthenticationFailure(request, response, e);
+            } finally {
+                OnSecurityManageContextHolder.clearContext();
+            }
+        }
+    }
+
+    /**
+     * 认证成功后的处理器
+     */
+    private static class ManageTokenExternalAccessAuthorizationSuccessHandler implements AuthenticationSuccessHandler {
+        private OnSecurityJsonMapper jsonMapper = new OnSecurityJsonMapper();
+
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+            ManageTokenAccessAuthorization accessAuthorization = (ManageTokenAccessAuthorization) authentication;
+            String responseJson = jsonMapper.writeValueAsString(accessAuthorization);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+            response.setContentType(MediaType.APPLICATION_JSON.toString());
+            response.getWriter().write(responseJson);
+        }
+    }
+}
